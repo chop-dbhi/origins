d2185f61875a7df8a30e052bc9d06c7a6d7b223b
Byron Ruth <b@devel.io>
2013-11-23 11:41:19 -0500

    Add initial tests for api components
    Add BackendTestCase to handle skipping tests if an unsupported backend
    is detected. The subclass 'backend_path' must be specified which is the
    absolute import path to the backend. The `backend` property will be set
    on the class for use throughout the tests.

--- tests/backends_test.py
+++ tests/backends_test.py
@@ -1,15 +1,32 @@
+import os
 import unittest
+from importlib import import_module
 from origins.exceptions import OriginsError
 from origins.backends.base import LAZY_ATTR
 
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
-class TestBase(unittest.TestCase):
+
+class BackendTestCase(unittest.TestCase):
+    backend_path = ''
+
+    def load_backend(self):
+        if not self.backend_path:
+            raise ValueError('backend_path must be defined')
+        try:
+            self.backend = import_module(self.backend_path)
+        except ImportError as e:
+            self.skipTest(unicode(e))
+
     def setUp(self):
-        from origins.backends import base
-        self.module = base
+        self.load_backend()
+
+
+class BaseTestBase(BackendTestCase):
+    backend_path = 'origins.backends.base'
 
     def test_node_default_state(self):
-        node = self.module.Node()
+        node = self.backend.Node()
 
         self.assertEqual(node.attrs, {})
         self.assertEqual(node.source, None)
@@ -29,9 +46,18 @@ class TestBase(unittest.TestCase):
         self.assertEqual(node.branches(), [])
         self.assertEqual(node.elements(), [])
 
-    def test_lazy_attributes(self):
-        class Lazy(self.module.Node):
-            lazy_attributes = ('foo', 'bar', 'baz')
+    def test_node_dict_behavior(self):
+        node = self.backend.Node()
+        node['a'] = 1
+        self.assertEqual(node.attrs['a'], 1)
+        self.assertEqual(node['a'], 1)
+        del node['a']
+        self.assertFalse('a' in node.attrs)
+        self.assertFalse('a' in node)
+
+    def test_node_lazy_attributes(self):
+        class Lazy(self.backend.Node):
+            lazy_attributes = ('foo', 'bar')
 
             def foo(self):
                 return 1
