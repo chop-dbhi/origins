4c47786425dac0be5e595ae51909af9b0e3a7308
Byron Ruth <b@devel.io>
2013-12-17 12:03:19 -0500

    Simplify and fix neo4j export to correctly traverse relationships

--- origins/io/neo4j.py
+++ origins/io/neo4j.py
@@ -3,38 +3,43 @@ from __future__ import unicode_literals, absolute_import
 from . import _cypher
 
 
-def prepare_rel(rel, factory, traverse=True):
+def prepare_rel(rel, factory):
     stmts = []
 
-    if not factory.hasref(rel.id):
-        if not factory.hasref(rel.start.id):
-            stmts.append(factory.merge_node(rel.start))
-        if not factory.hasref(rel.end.id):
-            stmts.append(factory.merge_node(rel.end))
-        if not factory.hasref(rel.id):
-            stmts.append(factory.merge_rel(rel))
+    if factory.hasref(rel.id):
+        return stmts
 
+    if not factory.hasref(rel.start.id):
+        stmts.append(factory.merge_node(rel.start))
+
+    if not factory.hasref(rel.end.id):
+        stmts.append(factory.merge_node(rel.end))
+
+    stmts.append(factory.merge_rel(rel))
     return stmts
 
 
 def prepare_node(node, factory, traverse=True):
-    if not factory.hasref(node.id):
-        # The relationship from the root.
-        for rel in node.relpath:
-            yield prepare_rel(rel, factory, traverse=traverse)
-        # Other relationships
+    stmts = []
+
+    if factory.hasref(node.id):
+        return stmts
+
+    for rel in node.relpath:
+        stmts.extend(prepare_rel(rel, factory))
+
+    if traverse:
         for rel in node.rels():
-            yield prepare_rel(rel, factory, traverse=traverse)
-    yield []
+            stmts.extend(prepare_node(rel.end, factory, traverse=traverse))
+            stmts.extend(prepare_rel(rel, factory))
+
+    return stmts
 
 
 def parse(node, traverse=True):
-    statements = []
     factory = _cypher.MergeFactory()
-
-    for stmts in prepare_node(node, factory, traverse=traverse):
-        statements.extend(stmts)
-    return '\n'.join(statements)
+    stmts = prepare_node(node, factory, traverse=traverse)
+    return '\n'.join(stmts)
 
 
 def export(node, uri=None, traverse=True, batch_size=100):
@@ -46,15 +51,8 @@ def export(node, uri=None, traverse=True, batch_size=100):
     `batch_size` is used to prevent constructing too large of a statement
     for the REST API to process (e.g. out of memory errors).
     """
-    statements = []
     factory = _cypher.MergeFactory()
 
-    for stmts in prepare_node(node, factory, traverse=traverse):
-        statements.extend(stmts)
-        if batch_size >= len(statements):
-            _cypher.send_request(statements, uri=uri)
-            factory.deref()
-            statements = []
-
-    if statements:
-        _cypher.send_request(statements, uri=uri)
+    stmts = prepare_node(node, factory, traverse=traverse)
+    if stmts:
+        _cypher.send_request(stmts, uri=uri)
