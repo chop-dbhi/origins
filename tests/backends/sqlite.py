647249c3763ed37d729ed4522b33725152dd2914
Byron Ruth <b@devel.io>
2013-12-28 19:30:01 -0500

    Update tests for API changes and additional coverage

--- tests/backends/sqlite.py
+++ tests/backends/sqlite.py
@@ -10,17 +10,11 @@ class SqliteTestBase(BackendTestCase):
         path = os.path.join(TEST_DATA_DIR, 'chinook.sqlite')
         self.client = self.backend.Client(path)
 
-    def test_client_methods(self):
-        self.assertEqual(self.client.version(), 32)
-        self.assertEqual(self.client.database_size(), 1067008)
-
     def test_tables(self):
         tables = self.client.tables()
         self.assertEqual(len(tables), 11)
         self.assertTrue('name' in tables[0])
         self.assertEqual(sorted(tables), tables)
-
-        self.assertEqual(self.client.table_count('Album'), 347)
 
     def test_columns(self):
         columns = self.client.columns('Album')
@@ -29,9 +23,3 @@ class SqliteTestBase(BackendTestCase):
 
         sorted_columns = sorted(columns, key=lambda x: x['index'])
         self.assertEqual(sorted_columns, columns)
-
-        count = self.client.column_unique_count('Album', 'ArtistId')
-        self.assertEqual(count, 204)
-
-        values = list(self.client.column_unique_values('Album', 'ArtistId'))
-        self.assertEqual(len(values), 204)
--- tests/graph.py
+++ tests/graph.py
@@ -9,6 +9,12 @@ class GraphTestCase(unittest.TestCase):
         self.n2 = Node()
         self.n3 = Node()
 
+    def test_props(self):
+        self.n0['one'] = 1
+        self.assertEqual(self.n0['one'], 1)
+        del self.n0['one']
+        self.assertFalse('one' in self.n0)
+
     def test_relate(self):
         r0 = self.n0.relate(self.n1, 'CONTAINS')
         r1 = self.n0.relate(self.n1, 'RELATED')
@@ -17,24 +23,35 @@ class GraphTestCase(unittest.TestCase):
 
         self.assertTrue(self.n0.related(self.n1))
         self.assertTrue(self.n0.related(self.n2))
-        self.assertTrue(self.n0.related(self.n3))
+        self.assertTrue(self.n0.related(self.n3, 'RELATED'))
 
-        self.assertEqual(self.n0.rels(), [r0, r1, r2, r3])
+        self.assertEqual(sorted(self.n0.rels()), [r0, r1, r2, r3])
         self.assertEqual(self.n0.rels(node=self.n1), [r0, r1])
         self.assertEqual(self.n0.rels(type='CONTAINS'), [r0, r2])
         self.assertEqual(self.n0.rels(node=self.n1, type='RELATED'), [r1])
         self.assertEqual(self.n0.rels(node=self.n0, type='FOOBAR'), [])
 
-    def test_unrelate(self):
+    def test_relate_type_once(self):
+        r0 = self.n0.relate(self.n1, 'CONTAINS')
+        r0p = self.n0.relate(self.n1, 'CONTAINS', {'one': 1})
+        self.assertEqual(r0p, r0)
+        self.assertEqual(r0p.props, {'one': 1})
+
+    def test_unrelate_node(self):
         self.n0.relate(self.n1, 'CONTAINS')
         self.n0.relate(self.n1, 'RELATED')
         self.n0.relate(self.n2, 'CONTAINS')
-        self.n0.relate(self.n3, 'RELATED')
+        self.n0.relate(self.n2, 'RELATED')
+        self.n0.relate(self.n3, 'CONTAINS')
 
-        self.assertEqual(self.n0.unrelate(self.n1, 'RELATED'), 1)
-        self.assertEqual(self.n0.unrelate(self.n2, 'CONTAINS'), 1)
-        self.assertEqual(self.n0.unrelate(self.n2), 0)
-        self.assertEqual(self.n0.unrelate(), 2)
+        # Node
+        self.assertEqual(self.n0.unrelate(self.n1), 2)
+        # Type
+        self.assertEqual(self.n0.unrelate(type='RELATED'), 1)
+        # Node and Type
+        self.assertEqual(self.n0.unrelate(self.n3, type='CONTAINS'), 1)
+        # All
+        self.assertEqual(self.n0.unrelate(), 1)
 
         self.assertFalse(self.n0.related(self.n1))
         self.assertFalse(self.n0.related(self.n2))
@@ -56,3 +73,24 @@ class GraphTestCase(unittest.TestCase):
 
         # Arbitrary filter function
         self.assertEqual(rels.filter(lambda r: r.id % 2 == 0), [r0])
+
+        # Only functions and strings
+        self.assertRaises(TypeError, rels.filter, 10)
+
+    def test_nodes(self):
+        self.n0.relate(self.n1, 'CONTAINS')
+        self.n0.relate(self.n1, 'RELATED')
+        self.n0.relate(self.n2, 'CONTAINS')
+        self.n0.relate(self.n2, 'RELATED')
+        self.n0.relate(self.n3, 'CONTAINS')
+
+        # Get all relationships from n0
+        rels = self.n0.rels()
+        nodes = rels.nodes()
+        # Gets distinct set of end nodes of rels
+        self.assertEqual(nodes, [self.n1, self.n2, self.n3])
+
+        # Indexed by their bytes representation
+        self.assertEqual(nodes['1'], self.n1)
+
+        self.assertEqual(nodes.match(r'1|2'), [self.n1, self.n2])
