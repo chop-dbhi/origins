0b02a2bc518d7ee76af9eb1b4203ae691cf6b851
Byron Ruth <b@devel.io>
2013-12-29 20:47:41 -0500

    Add additional SQLite client tests

--- tests/backends/sqlite.py
+++ tests/backends/sqlite.py
@@ -2,13 +2,21 @@ import os
 from .base import BackendTestCase, TEST_DATA_DIR
 
 
-class SqliteTestBase(BackendTestCase):
+class SqliteClientTestCase(BackendTestCase):
     backend_path = 'origins.backends.sqlite'
 
     def setUp(self):
         self.load_backend()
         path = os.path.join(TEST_DATA_DIR, 'chinook.sqlite')
         self.client = self.backend.Client(path)
+
+    def test_properties(self):
+        self.assertFalse(self.client.foreign_keys_supported())
+
+    def test_database(self):
+        props = self.client.database()
+        self.assertGreater(props['size'], 0)
+        self.assertEqual(props['version'], 32)
 
     def test_tables(self):
         tables = self.client.tables()
@@ -23,3 +31,7 @@ class SqliteTestBase(BackendTestCase):
 
         sorted_columns = sorted(columns, key=lambda x: x['index'])
         self.assertEqual(sorted_columns, columns)
+
+    def test_foreign_keys(self):
+        fks = self.client.foreign_keys('Album', 'ArtistId')
+        self.assertEqual(len(fks), 1)
