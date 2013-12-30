31ad0aae6d9cb0ebe963c31e7e68338086368230
Byron Ruth <b@devel.io>
2013-12-30 10:21:59 -0500

    Add noop tests

---
+++ tests/backends/noop.py
@@ -0,0 +1,12 @@
+from .base import BackendTestCase
+
+
+class NoopClientTestCase(BackendTestCase):
+    backend_path = 'origins.backends.noop'
+
+    def setUp(self):
+        self.load_backend()
+        self.client = self.backend.Client()
+
+    def test(self):
+        self.assertTrue(self.client)
