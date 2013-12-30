31a10c57b94d4ba75f7475b4df91ccf75a102af8
Byron Ruth <b@devel.io>
2013-12-29 21:19:58 -0500

    Add additional delimited client test and node test

--- tests/backends/delimited.py
+++ tests/backends/delimited.py
@@ -11,10 +11,21 @@ class DelimitedBackendClientTestCase(BackendTestCase):
         path = os.path.join(TEST_DATA_DIR, 'chinook_tracks.csv')
         self.client = self.backend.Client(path)
 
+    def test_reader(self):
+        row = next(self.client.reader)
+        # Ensure the header is skipped
+        self.assertNotEqual(row[0], 'TrackId')
+
     def test_setup(self):
         self.assertTrue(self.client.has_header)
         self.assertEqual(self.client.delimiter, ',')
         self.assertEqual(len(self.client._header_index), 9)
+
+    def test_properties(self):
+        self.assertGreater(self.client.file_line_count(), 0)
+
+    def test_file(self):
+        self.assertTrue(self.client.file())
 
     def test_columns(self):
         columns = self.client.columns()
@@ -29,3 +40,7 @@ class DelimitedBackendTestCase(BackendTestCase):
         self.load_backend()
         path = os.path.join(TEST_DATA_DIR, 'chinook_tracks.csv')
         self.origin = origins.connect('delimited', path=path)
+
+    def test_sync(self):
+        self.assertTrue(self.origin.props)
+        self.assertTrue(self.origin.columns)
