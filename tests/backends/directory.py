183b7f1cf2cdbe5c67539ee9b927f56eb2989c58
Byron Ruth <b@devel.io>
2013-12-30 08:55:30 -0500

    Change directory tests to conform to client/API test case layout

--- tests/backends/directory.py
+++ tests/backends/directory.py
@@ -21,22 +21,37 @@ class DirectoryClientTestCase(BackendTestCase):
         self.assertGreater(files, 0)
 
 
-class DirectoryNodeTestCase(BackendTestCase):
+class DirectoryApiTestCase(BackendTestCase):
     backend_path = 'origins.backends.directory'
 
     def setUp(self):
-        self.load_backend()
-        self.origin = origins.connect('directory', path=TEST_DATA_DIR)
+        self.directory = origins.connect('directory', path=TEST_DATA_DIR)
 
-    def test_sync(self):
-        self.assertTrue(self.origin.props)
-        self.assertTrue(self.origin.files)
+    def test_directory(self):
+        self.assertEqual(self.directory.label, 'data')
+        self.assertEqual(self.directory.name, 'data')
+        self.assertEqual(self.directory.path, 'data')
+        self.assertEqual(self.directory.uri, 'directory:///data')
+        self.assertEqual(self.directory.relpath, [])
+        self.assertTrue(self.directory.isroot)
+        self.assertFalse(self.directory.isleaf)
+        self.assertTrue('uri' in self.directory.serialize())
+
+    def test_fields(self):
+        column = self.directory.files['chinook.sqlite']
+
+        self.assertEqual(column.label, 'chinook.sqlite')
+        self.assertEqual(column.name, 'chinook.sqlite')
+        self.assertEqual(column.path, 'data/chinook.sqlite')
+        self.assertEqual(len(column.relpath), 1)
+        self.assertFalse(column.isroot)
+        self.assertTrue(column.isleaf)
 
     def test_no_recurse(self):
-        node = origins.connect('directory', path=TEST_DATA_DIR, recurse=False)
+        d = origins.connect('directory', path=TEST_DATA_DIR, recurse=False)
         # Dirty way of ensuring the walk did not recurse
-        self.assertFalse([f for f in node.files if '/' in f['name']])
+        self.assertFalse([f for f in d.files if '/' in f['name']])
 
     def test_explicit_depth(self):
-        node = origins.connect('directory', path=TEST_DATA_DIR, depth=1)
-        self.assertTrue([f for f in node.files if '/' in f['name']])
+        d = origins.connect('directory', path=TEST_DATA_DIR, depth=1)
+        self.assertTrue([f for f in d.files if '/' in f['name']])
