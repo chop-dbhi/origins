3d5e1c03a52048179298d07aa14330249ddc6d36
Byron Ruth <b@devel.io>
2014-01-02 11:16:16 -0500

    Add tests for REDCap backends
    The MySQL version will be skipped unless a host is provided via the
    REDCAP_MYSQL_HOST environment variable.

---
+++ tests/backends/redcap_api.py
@@ -0,0 +1,91 @@
+import origins
+from .base import BackendTestCase
+
+API_TOKEN = '54C661E0BAFFF731BBE3C642B8277E45'
+API_URL = 'https://tiu.research.chop.edu/redcap/redcap/api/'
+
+
+class RedcapApiClientTestCase(BackendTestCase):
+    backend_path = 'origins.backends.redcap_api'
+
+    def setUp(self):
+        self.load_backend()
+        self.client = self.backend.Client('redcap_demo', url=API_URL,
+                                          token=API_TOKEN)
+
+    def test_project(self):
+        self.assertTrue(self.client.project())
+
+    def test_forms(self):
+        forms = self.client.forms()
+        self.assertEqual(len(forms), 11)
+        self.assertTrue('name' in forms[0])
+
+    def test_sections(self):
+        sections = self.client.sections('demographics')
+        self.assertEqual(len(sections), 5)
+        self.assertTrue('name' in sections[0])
+
+    def test_fields(self):
+        fields = self.client.fields('demographics', 'default')
+        self.assertEqual(len(fields), 3)
+        self.assertTrue('name' in fields[0])
+
+
+class RedcapApiApiTestCase(BackendTestCase):
+    backend_path = 'origins.backends.redcap_api'
+
+    def setUp(self):
+        self.project = origins.connect('redcap-api', name='redcap_demo',
+                                       url=API_URL, token=API_TOKEN)
+
+    def test_project(self):
+        self.assertEqual(self.project.label, 'redcap_demo')
+        self.assertEqual(self.project.name, 'redcap_demo')
+        self.assertEqual(self.project.path, 'redcap_demo')
+        self.assertEqual(self.project.uri,
+                         'redcap_api://tiu.research.chop.edu/redcap_demo')
+        self.assertEqual(self.project.relpath, [])
+        self.assertTrue(self.project.isroot)
+        self.assertFalse(self.project.isleaf)
+        self.assertTrue('uri' in self.project.serialize())
+
+    def test_form(self):
+        form = self.project.forms['demographics']
+
+        self.assertEqual(form.label, 'demographics')
+        self.assertEqual(form.name, 'demographics')
+        self.assertEqual(form.path, 'redcap_demo/demographics')
+        self.assertEqual(len(form.relpath), 1)
+        self.assertFalse(form.isroot)
+        self.assertFalse(form.isleaf)
+
+        # Sections are not required in a form. This is direct access to the
+        # fields.
+        self.assertGreater(form.fields, 0)
+
+    def test_section(self):
+        section = self.project.forms['demographics'].sections['default']
+
+        self.assertEqual(section.label, 'default')
+        self.assertEqual(section.name, 'default')
+        self.assertEqual(section.path, 'redcap_demo/demographics/default')
+        self.assertEqual(len(section.relpath), 2)
+        self.assertFalse(section.isroot)
+        self.assertFalse(section.isleaf)
+
+    def test_field(self):
+        field = self.project.forms['demographics'].sections['default']\
+            .fields['date_enrolled']
+
+        self.assertEqual(field.label, 'Date subject signed consent')
+        self.assertEqual(field.name, 'date_enrolled')
+        self.assertEqual(field.path, 'redcap_demo/demographics/default/'
+                                     'date_enrolled')
+        self.assertEqual(len(field.relpath), 3)
+        self.assertFalse(field.isroot)
+        self.assertTrue(field.isleaf)
+
+    def test_field_choices(self):
+        field = self.project.forms['demographics'].fields['race']
+        self.assertTrue(field.choices)
---
+++ tests/backends/redcap_csv.py
@@ -0,0 +1,90 @@
+import os
+import origins
+from .base import BackendTestCase, TEST_DATA_DIR
+
+FILENAME = 'REDCapDemoDatabase_DataDictionary_2013-12-12.csv'
+
+
+class RedcapCsvClientTestCase(BackendTestCase):
+    backend_path = 'origins.backends.redcap_csv'
+
+    def setUp(self):
+        self.load_backend()
+        path = os.path.join(TEST_DATA_DIR, FILENAME)
+        self.client = self.backend.Client(path)
+
+    def test_project(self):
+        self.assertTrue(self.client.project())
+
+    def test_forms(self):
+        forms = self.client.forms()
+        self.assertEqual(len(forms), 11)
+        self.assertTrue('name' in forms[0])
+
+    def test_sections(self):
+        sections = self.client.sections('demographics')
+        self.assertEqual(len(sections), 5)
+        self.assertTrue('name' in sections[0])
+
+    def test_fields(self):
+        fields = self.client.fields('demographics', 'default')
+        self.assertEqual(len(fields), 3)
+        self.assertTrue('name' in fields[0])
+
+
+class RedcapCsvApiTestCase(BackendTestCase):
+    backend_path = 'origins.backends.redcap_csv'
+
+    def setUp(self):
+        path = os.path.join(TEST_DATA_DIR, FILENAME)
+        self.project = origins.connect('redcap-csv', path=path)
+
+    def test_project(self):
+        self.assertEqual(self.project.label, FILENAME)
+        self.assertEqual(self.project.name, FILENAME)
+        self.assertEqual(self.project.path, FILENAME)
+        self.assertEqual(self.project.uri, 'redcap_csv:///' + FILENAME)
+        self.assertEqual(self.project.relpath, [])
+        self.assertTrue(self.project.isroot)
+        self.assertFalse(self.project.isleaf)
+        self.assertTrue('uri' in self.project.serialize())
+
+    def test_form(self):
+        form = self.project.forms['demographics']
+
+        self.assertEqual(form.label, 'demographics')
+        self.assertEqual(form.name, 'demographics')
+        self.assertEqual(form.path, FILENAME + '/demographics')
+        self.assertEqual(len(form.relpath), 1)
+        self.assertFalse(form.isroot)
+        self.assertFalse(form.isleaf)
+
+        # Sections are not required in a form. This is direct access to the
+        # fields.
+        self.assertGreater(form.fields, 0)
+
+    def test_section(self):
+        section = self.project.forms['demographics'].sections['default']
+
+        self.assertEqual(section.label, 'default')
+        self.assertEqual(section.name, 'default')
+        self.assertEqual(section.path, FILENAME + '/demographics/default')
+        self.assertEqual(len(section.relpath), 2)
+        self.assertFalse(section.isroot)
+        self.assertFalse(section.isleaf)
+
+    def test_field(self):
+        field = self.project.forms['demographics'].sections['default']\
+            .fields['date_enrolled']
+
+        self.assertEqual(field.label, 'Date subject signed consent')
+        self.assertEqual(field.name, 'date_enrolled')
+        self.assertEqual(field.path, FILENAME +
+                         '/demographics/default/date_enrolled')
+        self.assertEqual(len(field.relpath), 3)
+        self.assertFalse(field.isroot)
+        self.assertTrue(field.isleaf)
+
+    def test_field_choices(self):
+        field = self.project.forms['demographics'].fields['race']
+        self.assertTrue(field.choices)
