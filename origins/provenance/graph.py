bfa17e2d070c121a09a5f502112afff1fff6cd37
Byron Ruth <b@devel.io>
2014-06-12 08:10:57 -0400

    Implement (rough) provenance `sync_resource` function
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/provenance/graph.py
+++ origins/provenance/graph.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import time
 import logging
 import hashlib
-from origins.graph import neo4j, SHORT_SHA1_LEN
+from origins.graph import neo4j, utils, SHORT_SHA1_LEN
 from .loader import prepare_statements, parse_prov_data
 
 
@@ -17,14 +17,9 @@ logger = logging.getLogger(__name__)
 
 
 RESOURCE_PROV_NODES = '''\
-MATCH (n:`origins:Provenance:{sha1}`)
-RETURN id(n), n
-'''
-
-RESOURCE_PROV_LATEST_REVISION_NODES = '''\
-MATCH (:`origins:Provenance:{sha1}`)<-[:`prov:generalEntity`]-(:`origins:Specialization`)-[:`prov:specificEntity`]->(n)
-OPTIONAL MATCH (n)<-[:`prov:generatedEntity`*]-(:`origins:Derivation`)
-RETURN id(n), n
+MATCH (g:`origins:Provenance:{sha1}`)<-[:`prov:generalEntity`]-(:`prov:Specialization`)-[:`prov:specificEntity`]->(n)
+OPTIONAL MATCH (n)<-[:`prov:generatedEntity`*]-(:`prov:Derivation`)
+RETURN id(g), g, id(n), n
 '''  # noqa
 
 
@@ -36,6 +31,11 @@ OPTIONAL MATCH (n)-[r]-()
 DELETE r, n
 RETURN count(distinct n), count(distinct r)
 '''
+
+
+def _get_latest_revisions(short_sha1, tx=None):
+    query = RESOURCE_PROV_NODES.format(sha1=short_sha1)
+    return tx.send(query)
 
 
 def create_resource(data, tx=None):
@@ -64,7 +64,11 @@ def create_resource(data, tx=None):
         }
 
         # Revision
-        entity[oid_rev] = dict(node.get('properties', {}))
+        entity[oid_rev] = {
+            'origins:id': oid,
+        }
+
+        entity[oid_rev].update(node.get('properties', {}))
 
         # Generation
         generation[oid_gen] = {
@@ -90,7 +94,11 @@ def create_resource(data, tx=None):
         }
 
         # Revision
-        entity[oid_rev] = dict(rel.get('properties', {}))
+        entity[oid_rev] = {
+            'origins:id': oid,
+        }
+
+        entity[oid_rev].update(rel.get('properties', {}))
 
         generation[oid_gen] = {
             'prov:entity': oid_rev,
@@ -124,6 +132,201 @@ def create_resource(data, tx=None):
         'prov:specializationOf': len(specialization),
         'prov:wasGeneratedBy': len(generation),
         'origins:wasDependentOn': len(dependence),
+    }
+
+
+def sync_resource(data, add=True, remove=True, update=True, tx=None):
+    """Syncs client exported data with the existing resource graph.
+
+    `add` - Adds new nodes/rels that are not present in the existing graph.
+    `remove` - Remove old nodes/rels that are not present in `data`.
+    `update` - Merges changes in new nodes/rels into their existing nodes/rels.
+    """
+    if tx is None:
+        tx = neo4j
+
+    t0 = time.time()
+
+    data_entities = {}
+    data_entities.update(data.get('nodes', {}))
+    data_entities.update(data.get('relationships', {}))
+
+    nodes = set()
+
+    add_nodes = 0
+    update_nodes = 0
+    remove_nodes = 0
+
+    add_rels = 0
+    update_rels = 0
+    remove_rels = 0
+
+    entity = {}
+    entity_refs = {}
+    specialization = {}
+    generation = {}
+    invalidation = {}
+    dependence = {}
+
+    short_sha1 = hashlib.sha1(data['resource']).hexdigest()[:SHORT_SHA1_LEN]
+
+    # ID of general entity and latest revision id and properties
+    for gid, gprops, rid, old in _get_latest_revisions(short_sha1, tx):
+        oid = gprops['origins:id']
+
+        oid_old = oid + '_old'
+        oid_rev = oid + '_rev'
+        oid_gen = oid + '_gen'
+        oid_inv = oid + '_inv'
+        oid_spec = oid + '_spec'
+        oid_dep = oid + '_dep'
+
+        entity_refs[oid] = {
+            'origins:neo4j': gid,
+        }
+
+        # Current revision is the old and new revision
+        entity_refs[oid_old] = {
+            'origins:neo4j': rid,
+        }
+
+        entity_refs[oid_rev] = {
+            'origins:neo4j': rid,
+        }
+
+        new = data_entities.get(oid)
+        isrel = 'start' in new
+
+        # No longer exists
+        if remove and not new:
+            if isrel:
+                remove_rels += 1
+            else:
+                remove_nodes += 1
+
+            invalidation[oid_inv] = {
+                'prov:entity': oid_old
+            }
+        elif update:
+            diff = utils.diff(new.get('properties'), old)
+
+            # Changes have been made
+            if diff:
+                if isrel:
+                    update_rels += 1
+                else:
+                    update_nodes += 1
+
+                # New revision entity
+                entity[oid_rev] = {
+                    'origins:id': oid,
+                }
+
+                entity[oid_rev].update(new.get('properties', {}))
+
+                # Invalidate previous revision
+                invalidation[oid_inv] = {
+                    'prov:entity': oid_old
+                }
+
+                # Generation of new entity
+                generation[oid_gen] = {
+                    'prov:entity': oid_rev,
+                }
+
+                # Specialization
+                specialization[oid_spec] = {
+                    'prov:generalEntity': oid,
+                    'prov:specificEntity': oid_rev,
+                }
+
+                if isrel:
+                    # Dependence
+                    dependence[oid_dep] = {
+                        'origins:dependent': new['start'] + '_rev',
+                        'origins:dependency': new['end'] + '_rev',
+                    }
+
+        nodes.add(oid)
+
+    # All nodes/rels that have been unseen are marked for creation
+    if add:
+        for oid, new in data_entities.items():
+            isrel = 'start' in new
+
+            if oid not in nodes:
+                if isrel:
+                    add_rels += 1
+                else:
+                    add_nodes += 1
+
+                oid_rev = oid + '_rev'
+                oid_gen = oid + '_gen'
+                oid_spec = oid + '_spec'
+                oid_dep = oid + '_dep'
+
+                # Permanode
+                entity[oid] = {
+                    'origins:id': oid,
+                }
+
+                # Revision
+                entity[oid_rev] = {
+                    'origins:id': oid,
+                }
+
+                entity[oid_rev].update(new.get('properties', {}))
+
+                # Generation
+                generation[oid_gen] = {
+                    'prov:entity': oid_rev,
+                }
+
+                # Specialization
+                specialization[oid_spec] = {
+                    'prov:generalEntity': oid,
+                    'prov:specificEntity': oid_rev,
+                }
+
+                if isrel:
+                    # Dependence
+                    dependence[oid_dep] = {
+                        'origins:dependent': new['start'] + '_rev',
+                        'origins:dependency': new['end'] + '_rev',
+                    }
+
+    new_entities = len(entity)
+    entity.update(entity_refs)
+
+    bundles = parse_prov_data({
+        'prov:entity': entity,
+        'prov:specializationOf': specialization,
+        'prov:wasGeneratedBy': generation,
+        'prov:wasInvalidatedBy': invalidation,
+        'origins:wasDependentOn': dependence,
+    })
+
+    statements = prepare_statements(bundles, short_sha1=short_sha1)
+
+    tx.send(statements)
+
+    return {
+        'time': time.time() - t0,
+        'prov:entity': new_entities,
+        'prov:specializationOf': len(specialization),
+        'prov:wasGeneratedBy': len(generation),
+        'prov:wasInvalidatedBy': len(invalidation),
+        'origins:wasDependentOn': len(dependence),
+        'nodes': {
+            'added': add_nodes,
+            'updated': update_nodes,
+            'removed': remove_nodes,
+        },
+        'relationships': {
+            'added': add_rels,
+            'updated': update_rels,
+            'removed': remove_rels,
+        },
     }
 
 
--- tests/provenance/graph.py
+++ tests/provenance/graph.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import os
 import json
 import unittest
-from origins import provenance
+from origins.provenance import graph
 from origins.graph import neo4j
 from .. import TEST_DATA_DIR
 
@@ -22,16 +22,40 @@ class GraphTestCase(unittest.TestCase):
         neo4j.purge()
 
     def test_create_resource(self):
-        out = provenance.graph.create_resource(self.data)
+        out = graph.create_resource(self.data)
 
         self.assertEqual(out['prov:entity'], 324)
         self.assertEqual(out['prov:specializationOf'], 162)
         self.assertEqual(out['prov:wasGeneratedBy'], 162)
         self.assertEqual(out['origins:wasDependentOn'], 86)
 
+    def test_sync_resource(self):
+        out = graph.sync_resource(self.data)
+
+        self.assertEqual(out['prov:entity'], 324)
+        self.assertEqual(out['prov:specializationOf'], 162)
+        self.assertEqual(out['prov:wasGeneratedBy'], 162)
+        self.assertEqual(out['origins:wasDependentOn'], 86)
+
+        out = graph.sync_resource(self.data)
+
+        self.assertEqual(out['prov:specializationOf'], 0)
+        self.assertEqual(out['prov:wasGeneratedBy'], 0)
+        self.assertEqual(out['prov:wasInvalidatedBy'], 0)
+        self.assertEqual(out['origins:wasDependentOn'], 0)
+
+        self.data['nodes']['chinook.sqlite/Album/Title']['properties']['type'] = 'NVARCHAR(200)'  # noqa
+
+        out = graph.sync_resource(self.data)
+
+        self.assertEqual(out['prov:specializationOf'], 1)
+        self.assertEqual(out['prov:wasGeneratedBy'], 1)
+        self.assertEqual(out['prov:wasInvalidatedBy'], 1)
+        self.assertEqual(out['origins:wasDependentOn'], 0)
+
     def test_delete_resource(self):
-        provenance.graph.create_resource(self.data)
-        out = provenance.graph.delete_resource(self.data)
+        graph.create_resource(self.data)
+        out = graph.delete_resource(self.data)
 
         self.assertEqual(out['nodes'], 734)
         self.assertEqual(out['relationships'], 658)
