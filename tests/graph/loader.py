91e3796835a2e4be1eb4fcd5bc03d9cd2f793b98
Byron Ruth <b@devel.io>
2014-06-12 08:11:50 -0400

    Add additional tests for graph sync_resource
    Signed-off-by: Byron Ruth <b@devel.io>

--- tests/graph/loader.py
+++ tests/graph/loader.py
@@ -29,8 +29,41 @@ class GraphTestCase(unittest.TestCase):
     def test_sync_resource(self):
         out = graph.sync_resource(self.data)
 
-        self.assertEqual(out['nodes']['added'], 76)
-        self.assertEqual(out['relationships']['added'], 86)
+        self.assertEqual(out['nodes'], {
+            'added': 76,
+            'updated': 0,
+            'removed': 0,
+        })
+
+        self.assertEqual(out['relationships'], {
+            'added': 86,
+            'updated': 0,
+            'removed': 0,
+        })
+
+        out = graph.sync_resource(self.data)
+
+        self.assertEqual(out['nodes'], {
+            'added': 0,
+            'updated': 0,
+            'removed': 0,
+        })
+
+        self.assertEqual(out['relationships'], {
+            'added': 0,
+            'updated': 0,
+            'removed': 0,
+        })
+
+        self.data['nodes']['chinook.sqlite/Album/Title']['properties']['type'] = 'NVARCHAR(200)'  # noqa
+
+        out = graph.sync_resource(self.data)
+
+        self.assertEqual(out['nodes'], {
+            'added': 0,
+            'updated': 1,
+            'removed': 0,
+        })
 
     def test_delete_resource(self):
         graph.create_resource(self.data)
