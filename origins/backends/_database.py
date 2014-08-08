815a1dea7113d1df0b9079bddba69ffd5ada10e0
Byron Ruth <b@devel.io>
2014-08-08 07:13:22 -0400

    Remove DAL components
    The data-access layer components is out of scope for the core since it
    is a use case of the resulting graph rather than contributing to it's
    formation.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/backends/_database.py
+++ origins/backends/_database.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals, absolute_import
 
 import re
 import logging
-from ..dal import recordtuple
 from . import base
 
 try:
@@ -49,31 +48,6 @@ class Client(base.Client):
         if row:
             return row[0]
 
-    def _select_query(self, select, columns, table, orderby=''):
-        return '{select} {columns} FROM {table} {orderby}'\
-               .format(select=select, columns=columns, table=table,
-                       orderby=orderby).strip()
-
-    def _results_iter(self, names, query, unpack):
-        # Not used below if unpack is true
-        if unpack:
-            record = None
-        else:
-            record = recordtuple(names)
-
-        c = self.connection.cursor()
-        c.execute(query)
-        batch = c.fetchmany()
-
-        while batch:
-            for row in batch:
-                if unpack:
-                    yield row[0]
-                else:
-                    yield record(*row)
-
-            batch = c.fetchmany()
-
     def _parse_dtype(self, dtype):
         match = re.match(r'^([a-z]+)', dtype, re.I)
         if match:
@@ -99,144 +73,6 @@ class Client(base.Client):
         dtype = self._parse_dtype(dtype)
         return dtype in self.BOOL_TYPES
 
-    def select(self, table_name, column_names, distinct=False,
-               sort=None, unpack=False, iterator=False):
-
-        # Handle sort direction for single column
-        if len(column_names) == 1 and isinstance(sort, str):
-            sort = [(column_names[0], sort)]
-
-        # Handle single sort, e.g. ('foo', 'desc')
-        elif sort and not isinstance(sort[0], (list, tuple)):
-            sort = [sort]
-
-        select = 'SELECT DISTINCT' if distinct else 'SELECT'
-        if column_names:
-            columns = ', '.join([self.qn(c) for c in column_names])
-        else:
-            columns = '*'
-        orderby = 'ORDER BY ' + ', '.join([' '.join([self.qn(c), d])
-                                          for c, d in sort]) if sort else ''
-
-        table = self.qn(table_name)
-        query = self._select_query(select, columns, table, orderby)
-
-        logger.debug(query)
-
-        _iterator = self._results_iter(column_names, query, unpack)
-
-        if iterator:
-            return _iterator
-
-        results = []
-        for record in _iterator:
-            results.append(record)
-        return tuple(results)
-
-    def count(self, table_name, column_names=None, distinct=False):
-        subquery = False
-
-        if column_names:
-            columns = ', '.join([self.qn(c) for c in column_names])
-            if len(column_names) > 1:
-                subquery = True
-        else:
-            columns = '*'
-            if distinct:
-                subquery = True
-
-        table = self.qn(table_name)
-
-        if subquery:
-            select = 'SELECT DISTINCT' if distinct else 'SELECT'
-            query = '''
-                SELECT COUNT(*) FROM ({subquery}) T
-            '''.format(subquery=self._select_query(select, columns, table))
-        else:
-            distinct = 'DISTINCT ' if distinct else ''
-            query = '''
-                SELECT COUNT({distinct}{columns}) FROM {table}
-            '''.format(distinct=distinct, columns=columns,
-                       table=self.qn(table_name))
-
-        logger.debug(query)
-        return self.fetchvalue(query)
-
-    def _aggregate(self, function, table_name, column_name):
-        table = self.qn(table_name)
-        column = self.qn(column_name)
-
-        query = '''
-            SELECT {function}({column})
-            FROM {table}
-        '''.format(function=function, column=column, table=table)
-
-        logger.debug(query)
-
-        return self.fetchvalue(query)
-
-    def min(self, table_name, column_name, dtype):
-        "Returns the minimum (lexicographic) value in the values."
-        if (self.is_numeric_type(dtype) or self.is_time_type(dtype) or
-                self.is_string_type(dtype)):
-            return self._aggregate('MIN', table_name, column_name)
-
-    def max(self, table_name, column_name, dtype):
-        "Returns the maximum (lexicographic) value in the values."
-        if (self.is_numeric_type(dtype) or self.is_time_type(dtype) or
-                self.is_string_type(dtype)):
-            return self._aggregate('MAX', table_name, column_name)
-
-    def avg(self, table_name, column_name, dtype):
-        "Returns the average of the values."
-        if self.is_numeric_type(dtype) or self.is_time_type(dtype):
-            return self._aggregate('AVG', table_name, column_name)
-
-    def sum(self, table_name, column_name, dtype):
-        "Returns the sum of values."
-        if self.is_numeric_type(dtype):
-            return self._aggregate('SUM', table_name, column_name)
-
-    def stddev(self, table_name, column_name, dtype):
-        "Returns the standard deviation of values."
-        if self.is_numeric_type(dtype) or self.is_time_type(dtype):
-            return self._aggregate('STDDEV', table_name, column_name)
-
-    def variance(self, table_name, column_name, dtype):
-        "Returns the variance of values."
-        if self.is_numeric_type(dtype) or self.is_time_type(dtype):
-            return self._aggregate('VARIANCE', table_name, column_name)
-
-    def longest(self, table_name, column_name, dtype):
-        "Returns the longest string."
-        if self.is_string_type(dtype):
-            table = self.qn(table_name)
-            column = self.qn(column_name)
-
-            query = '''
-                SELECT DISTINCT {column}
-                FROM {table},
-                ( SELECT MAX(length({column})) AS L FROM {table} ) T
-                WHERE length({column}) = T.L
-            '''.format(column=column, table=table)
-
-            return tuple(row[0] for row in self.fetchall(query))
-
-    def shortest(self, table_name, column_name, dtype):
-        "Returns the shortest string."
-        if self.is_string_type(dtype):
-            table = self.qn(table_name)
-            column = self.qn(column_name)
-
-            query = '''
-                SELECT DISTINCT {column}
-                FROM {table},
-                ( SELECT MIN(length({column})) AS L FROM {table} ) T
-                WHERE length({column}) = T.L
-            '''.format(column=column, table=table)
-
-            return tuple(row[0] for row in self.fetchall(query))
-
 
 class Database(base.Component):
     def sync(self):
@@ -255,26 +91,6 @@ class Table(base.Component):
     @property
     def columns(self):
         return self.definitions('column', sort='index')
-
-    def count(self, names=None, distinct=False):
-        """Returns a count of all records. If `distinct` is true, duplicate
-        records will not be counted. If `names` is not falsy, perform a
-        distinct count on only those data elements.
-        """
-        if not names:
-            names = [c['name'] for c in self.columns]
-        return self.client.count(self['name'], names, distinct=distinct)
-
-    def select(self, names=None, distinct=False, sort=None, iterator=True):
-        """Returns records from this table. `names` can be a list of element
-        names to select a subset of fields. `sort` can be a list of sort order
-        pairs of (name, direction), a single pair, or just the direction for
-        single column selects.
-        """
-        if not names:
-            names = [c['name'] for c in self.columns]
-        return self.client.select(self['name'], names, distinct=distinct,
-                                  sort=sort, iterator=iterator)
 
 
 class Column(base.Component):
@@ -299,52 +115,3 @@ class Column(base.Component):
             self._foreign_keys_synced = True
         return self.rels(type='REFERENCES', outgoing=True)\
             .filter('type', 'foreignkey').nodes()
-
-    def count(self, distinct=True):
-        """Returns the count of the values in this column. By default this
-        is a distinct count, but this can be toggled using the flag.
-        """
-        return self.client.count(self.parent['name'], [self['name']],
-                                 distinct=distinct)
-
-    def select(self, distinct=True, sort=None, iterator=True, unpack=False):
-        """Returns records for this column. The sort order can be specified by
-        setting `sort` to 'desc' or 'asc'. For convenience, the `unpack`
-        option can be set to true to return the value in the iterator rather
-        than within a record.
-        """
-        return self.client.select(self.parent['name'], [self['name']],
-                                  distinct=distinct, sort=sort,
-                                  iterator=iterator, unpack=unpack)
-
-    def min(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.min(table, column, dtype)
-
-    def max(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.max(table, column, dtype)
-
-    def sum(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.sum(table, column, dtype)
-
-    def avg(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.avg(table, column, dtype)
-
-    def stddev(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.stddev(table, column, dtype)
-
-    def variance(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.variance(table, column, dtype)
-
-    def longest(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.longest(table, column, dtype)
-
-    def shortest(self):
-        table, column, dtype = self.parent['name'], self['name'], self['type']
-        return self.client.shortest(table, column, dtype)
--- tests/backends/sqlite.py
+++ tests/backends/sqlite.py
@@ -4,8 +4,6 @@ import os
 import origins
 from .base import BackendTestCase, TEST_DATA_DIR
 
-import sqlite3
-
 
 class SqliteClientTestCase(BackendTestCase):
     backend_path = 'origins.backends.sqlite'
@@ -81,67 +79,3 @@ class SqliteApiTestCase(BackendTestCase):
     def test_foreign_keys(self):
         fks = self.db.tables['Album'].columns['ArtistId'].foreign_keys
         self.assertEqual(len(fks), 1)
-
-
-class SqliteDalTestCase(BackendTestCase):
-    backend_path = 'origins.backends.sqlite'
-
-    def setUp(self):
-        self.load_backend()
-        path = os.path.join(TEST_DATA_DIR, 'chinook.sqlite')
-        self.db = origins.connect('sqlite', path=path)
-
-    def test_table_count(self):
-        table = self.db.tables['Employee']
-        self.assertEqual(table.count(), 8)
-
-    def test_table_select(self):
-        table = self.db.tables['Employee']
-        records = table.select(['FirstName', 'LastName'], iterator=False)
-        self.assertTrue(records)
-        self.assertEqual(records[0], ('Andrew', 'Adams'))
-
-    def test_column_count(self):
-        column = self.db.tables['Employee'].columns['Title']
-        self.assertEqual(column.count(), 5)
-        self.assertEqual(column.count(distinct=False), 8)
-
-    def test_column_select(self):
-        column = self.db.tables['Employee'].columns['Title']
-        records = column.select(iterator=False)
-        self.assertTrue(records)
-
-    def test_column_sum(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertGreater(column.sum(), 0)
-
-    def test_column_avg(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertGreater(column.avg(), 0)
-
-    def test_column_min(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertGreater(column.min(), 0)
-
-    def test_column_max(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertGreater(column.max(), 0)
-
-    # SQLite does not have built-in functions for STDDEV nor VARIANCE, however
-    # they are available in a C-extension: http://www.sqlite.org/contrib
-    # therefore it _may_ work.
-    def test_column_stddev(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertRaises(sqlite3.OperationalError, column.stddev)
-
-    def test_column_variance(self):
-        column = self.db.tables['track'].columns['milliseconds']
-        self.assertRaises(sqlite3.OperationalError, column.variance)
-
-    def test_column_longest(self):
-        column = self.db.tables['track'].columns['name']
-        self.assertGreater(len(column.longest()), 0)
-
-    def test_column_shortest(self):
-        column = self.db.tables['track'].columns['name']
-        self.assertGreater(len(column.shortest()), 0)
