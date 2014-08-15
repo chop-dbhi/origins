984210ab22a5db597b5716d1d45819d097f83cb4
Byron Ruth <b@devel.io>
2014-08-15 15:25:17 -0400

    Add support for configuration file
    A JSON-based configuration file can be provided at startup time for the
    service, either with a command line flag `-c` or an environment variable
    `ORIGINS_CONFIG`.
    
    Any other environment variables take precedence which enables
    environment-specific overrides to a base configuration.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/service/__main__.py
+++ origins/service/__main__.py
@@ -1,15 +1,72 @@
 from __future__ import unicode_literals, absolute_import
 
 import os
+import sys
+import json
+from copy import deepcopy
 from origins.service.resources import app
 
-HOST = os.environ.get('ORIGINS_HOST', 'localhost')
-PORT = os.environ.get('ORIGINS_PORT', 5000)
-DEBUG = os.environ.get('ORIGINS_DEBUG')
 
-if DEBUG == '1':
-    debug = True
+# Default configuration options
+default_config = {
+    'debug': None,
+
+    'host': 'localhost',
+    'port': 5000,
+
+    'redis_port': 'localhost',
+    'redis_host': 6379,
+    'redis_db': 0,
+
+    'neo4j_host': 'localhost',
+    'neo4j_port': 7474,
+}
+
+
+config = deepcopy(default_config)
+
+
+# Fragile support for a configuration file
+if '-c' in sys.argv:
+    try:
+        config_name = sys.argv[sys.argv.index('-c') + 1]
+    except IndexError:
+        print('-c flag requires a path to a configuration file')
+        sys.exit(1)
 else:
-    debug = HOST in ('127.0.0.1', 'localhost')
+    config_name = os.environ.get('ORIGINS_CONFIG')
 
-app.run(debug=debug, threaded=True, host=HOST, port=int(PORT))
+
+# Update configuration if available
+if config_name:
+    try:
+        with open(config_name) as f:
+            try:
+                config.update(json.load(f))
+            except ValueError:
+                print('configuration file must be valid JSON')
+                sys.exit(1)
+    except IOError:
+        print('cannot read configuration file')
+        sys.exit(1)
+
+
+# Override with environment-specific variables
+for key in default_config:
+    var = 'ORIGINS_' + key.upper()
+
+    if var in os.environ:
+        value = os.environ[var]
+
+        try:
+            value = int(value)
+        except (ValueError, TypeError):
+            pass
+
+        config[key] = value
+
+
+app.run(threaded=True,
+        debug=config['debug'],
+        host=config['host'],
+        port=config['port'])
