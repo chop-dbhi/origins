05329de43a8b7d979650450cbdc5415955eb6f06
Byron Ruth <b@devel.io>
2014-08-19 14:47:30 -0400

    Add support for running origins.events as dequeue daemon
    Executing `python -m origins.events` will start a daemon that polls
    Redis and dequeues events from all registered topics.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/events.py
+++ origins/events.py
@@ -1,10 +1,15 @@
 import re
+import sys
 import time
 import json
 import uuid
 import inspect
 import logging
 import requests
+import traceback
+from datetime import datetime
+from concurrent import futures
+from multiprocessing import cpu_count
 from importlib import import_module
 from . import config
 from .exceptions import OriginsError
@@ -273,3 +278,62 @@ def unwatch(uuid, handler):
     key = '{}.{}'.format(ORIGINS_WATCHERS, uuid)
 
     r.srem(key, handler)
+
+
+if __name__ == '__main__':
+    r = get_redis_client()
+
+    if not r:
+        print('redis not configured')
+        sys.exit(1)
+
+    if '-q' in sys.argv:
+        quiet = True
+    else:
+        quiet = False
+
+    MAX_WORKERS = cpu_count()
+
+    sys.stdout.write('[{}] Started Origins events daemon ({} workers):\n'
+                     .format(datetime.now(), MAX_WORKERS))
+
+    def run(topics):
+        if not topics:
+            return
+
+        with futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as ex:
+            # Submit jobs to executor getting back "futures". A dict is
+            # used here to map the future to the topic name
+            topic_futures = {
+                ex.submit(dequeue, topic): topic
+                for topic in topics
+            }
+
+            # Get results as they are completed
+            for future in futures.as_completed(topic_futures):
+                topic = topic_futures[future]
+                now = datetime.now()
+
+                try:
+                    result = future.result()
+
+                    if not quiet and result:
+                        subscribers, watchers = result
+                        watchers = '<na>' if watchers is None else watchers
+                        sys.stdout.write('[{}] dequeued `{}` to {}/{}\n'
+                                         .format(now, topic, subscribers,
+                                                 watchers))
+                except Exception:
+                    sys.stderr.write('[{}] exception for `{}`:\n'
+                                     .format(now, topic))
+                    traceback.print_exc()
+
+    while True:
+        try:
+            topics = r.smembers(ORIGINS_TOPICS)
+            run(topics)
+        except KeyboardInterrupt:
+            break
+
+        # Wait..
+        time.sleep(1)
