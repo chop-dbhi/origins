bce1b1f1f136cd19b03235f2e5fb3e7aba63d938
Byron Ruth <b@devel.io>
2014-09-07 07:12:43 -0400

    Start origins service for Travis
    This relies on the provided origins script installed via setuptools.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- setup.py
+++ setup.py
@@ -7,19 +7,23 @@ if sys.version_info < (3, 3, 0):
 
 kwargs = {
     'name': 'origins',
+
     'version': __import__('origins').get_version(),
+
     'description': 'Origins is a free and open source service for building '
                    'information dependency graphs across your data, '
                    'systems, and operations.',
+
     'url': 'https://github.com/cbmi/origins/',
 
-    'packages': find_packages(exclude=['tests']),
-
     'author': 'Byron Ruth',
+
     'author_email': 'b@devel.io',
 
     'license': 'BSD',
+
     'keywords': 'graph dependency provenance information service REST',
+
     'classifiers': [
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
@@ -29,6 +33,8 @@ kwargs = {
         'Programming Language :: Python :: 3.4',
     ],
 
+    'packages': find_packages(exclude=['tests']),
+
     'install_requires': [
         'docopt>=0.6',
         'requests>=2.2.0,<2.3',
@@ -36,7 +42,9 @@ kwargs = {
         'flask>=0.10,<0.11',
         'flask-restful>=0.2,<0.3',
         'flask-cors>=1.7,<1.8',
-    ]
+    ],
+
+    'scripts': ['bin/origins'],
 }
 
 setup(**kwargs)
