332fac08680e6eeaf07e0eb5bfa74a5df5cc90a3
Byron Ruth <b@devel.io>
2014-09-07 07:03:43 -0400

    Fix flake8 errors
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/service/__init__.py
+++ origins/service/__init__.py
@@ -67,98 +67,98 @@ api.add_resource(nodes.Node,
                  '/nodes/<uuid>/',
                  endpoint='node')
 
-#api.add_resource(resources.ResourceRelationships,
-#                 '/resources/<uuid>/relationships/',
-#                 endpoint='resource-relationships')
+# api.add_resource(resources.ResourceRelationships,
+#                  '/resources/<uuid>/relationships/',
+#                  endpoint='resource-relationships')
 #
-#api.add_resource(resources.ResourceTimeline,
-#                 '/resources/<uuid>/timeline/',
-#                 endpoint='resource-timeline'),
+# api.add_resource(resources.ResourceTimeline,
+#                  '/resources/<uuid>/timeline/',
+#                  endpoint='resource-timeline'),
 #
-#api.add_resource(collections.Collections,
-#                 '/collections/',
-#                 endpoint='collections')
+# api.add_resource(collections.Collections,
+#                  '/collections/',
+#                  endpoint='collections')
 #
-#api.add_resource(collections.Collection,
-#                 '/collections/<uuid>/',
-#                 endpoint='collection')
+# api.add_resource(collections.Collection,
+#                  '/collections/<uuid>/',
+#                  endpoint='collection')
 #
-#api.add_resource(collections.CollectionResources,
-#                 '/collections/<uuid>/resources/',
-#                 endpoint='collection-resources')
+# api.add_resource(collections.CollectionResources,
+#                  '/collections/<uuid>/resources/',
+#                  endpoint='collection-resources')
 #
-#api.add_resource(components.ComponentSources,
-#                 '/components/<uuid>/sources/',
-#                 endpoint='component-sources')
+# api.add_resource(components.ComponentSources,
+#                  '/components/<uuid>/sources/',
+#                  endpoint='component-sources')
 #
-#api.add_resource(components.ComponentChildren,
-#                 '/components/<uuid>/children/',
-#                 endpoint='component-children')
+# api.add_resource(components.ComponentChildren,
+#                  '/components/<uuid>/children/',
+#                  endpoint='component-children')
 #
-#api.add_resource(components.ComponentLineage,
-#                 '/components/<uuid>/lineage/',
-#                 endpoint='component-lineage')
+# api.add_resource(components.ComponentLineage,
+#                  '/components/<uuid>/lineage/',
+#                  endpoint='component-lineage')
 #
-#api.add_resource(components.ComponentRevisions,
-#                 '/components/<uuid>/revisions/',
-#                 endpoint='component-revisions')
+# api.add_resource(components.ComponentRevisions,
+#                  '/components/<uuid>/revisions/',
+#                  endpoint='component-revisions')
 #
-#api.add_resource(components.ComponentRevision,
-#                 '/components/revisions/<uuid>/',
-#                 endpoint='component-revision')
+# api.add_resource(components.ComponentRevision,
+#                  '/components/revisions/<uuid>/',
+#                  endpoint='component-revision')
 #
-#api.add_resource(components.ComponentTimeline,
-#                 '/components/<uuid>/timeline/',
-#                 endpoint='component-timeline')
+# api.add_resource(components.ComponentTimeline,
+#                  '/components/<uuid>/timeline/',
+#                  endpoint='component-timeline')
 #
-#api.add_resource(components.ComponentRelationships,
-#                 '/components/<uuid>/relationships/',
-#                 endpoint='component-relationships')
+# api.add_resource(components.ComponentRelationships,
+#                  '/components/<uuid>/relationships/',
+#                  endpoint='component-relationships')
 #
-#api.add_resource(relationships.Relationships,
-#                 '/relationships/',
-#                 endpoint='relationships')
+# api.add_resource(relationships.Relationships,
+#                  '/relationships/',
+#                  endpoint='relationships')
 #
-#api.add_resource(relationships.Relationship,
-#                 '/relationships/<uuid>/',
-#                 endpoint='relationship')
+# api.add_resource(relationships.Relationship,
+#                  '/relationships/<uuid>/',
+#                  endpoint='relationship')
 #
-#api.add_resource(relationships.RelationshipRevisions,
-#                 '/relationships/<uuid>/revisions/',
-#                 endpoint='relationship-revisions')
+# api.add_resource(relationships.RelationshipRevisions,
+#                  '/relationships/<uuid>/revisions/',
+#                  endpoint='relationship-revisions')
 #
-#api.add_resource(relationships.RelationshipRevision,
-#                 '/relationships/revisions/<uuid>/',
-#                 endpoint='relationship-revision')
+# api.add_resource(relationships.RelationshipRevision,
+#                  '/relationships/revisions/<uuid>/',
+#                  endpoint='relationship-revision')
 #
-#api.add_resource(relationships.RelationshipTimeline,
-#                 '/relationships/<uuid>/timeline/',
-#                 endpoint='relationship-timeline')
+# api.add_resource(relationships.RelationshipTimeline,
+#                  '/relationships/<uuid>/timeline/',
+#                  endpoint='relationship-timeline')
 #
-#api.add_resource(trends.Trends,
-#                 '/trends/',
-#                 endpoint='trends')
+# api.add_resource(trends.Trends,
+#                  '/trends/',
+#                  endpoint='trends')
 #
-#api.add_resource(trends.ConnectedComponentsTrend,
-#                 '/trends/connected-components/',
-#                 endpoint='trend-components-connected')
+# api.add_resource(trends.ConnectedComponentsTrend,
+#                  '/trends/connected-components/',
+#                  endpoint='trend-components-connected')
 #
-#api.add_resource(trends.UsedComponentsTrend,
-#                 '/trends/used-components/',
-#                 endpoint='trend-used-components')
+# api.add_resource(trends.UsedComponentsTrend,
+#                  '/trends/used-components/',
+#                  endpoint='trend-used-components')
 #
-#api.add_resource(trends.ConnectedResourcesTrend,
-#                 '/trends/connected-resources/',
-#                 endpoint='trend-connected-resources')
+# api.add_resource(trends.ConnectedResourcesTrend,
+#                  '/trends/connected-resources/',
+#                  endpoint='trend-connected-resources')
 #
-#api.add_resource(trends.UsedResourcesTrend,
-#                 '/trends/used-resources/',
-#                 endpoint='trend-used-resources')
+# api.add_resource(trends.UsedResourcesTrend,
+#                  '/trends/used-resources/',
+#                  endpoint='trend-used-resources')
 #
-#api.add_resource(trends.ComponentSourcesTrend,
-#                 '/trends/component-sources/',
-#                 endpoint='trend-component-sources')
+# api.add_resource(trends.ComponentSourcesTrend,
+#                  '/trends/component-sources/',
+#                  endpoint='trend-component-sources')
 #
-#api.add_resource(trends.CommonRelationshipsTrend,
-#                 '/trends/common-relationships/',
-#                 endpoint='trend-common-relationships')
+# api.add_resource(trends.CommonRelationshipsTrend,
+#                  '/trends/common-relationships/',
+#                  endpoint='trend-common-relationships')
--- origins/service/utils.py
+++ origins/service/utils.py
@@ -28,10 +28,6 @@ def add_resource_data(n):
             'href': url_for('resource-components', uuid=n['uuid'],
                             _external=True),
         },
-#        'timeline': {
-#            'href': url_for('resource-timeline', uuid=n.uuid,
-#                            _external=True),
-#        }
     }
 
     return n
@@ -45,18 +41,6 @@ def add_component_data(n):
             'href': url_for('component', uuid=n['uuid'],
                             _external=True),
         },
-#        'relationships': {
-#            'href': url_for('component-relationships', uuid=uuid,
-#                            _external=True),
-#        },
-#        'revisions': {
-#            'href': url_for('component-revisions', uuid=uuid,
-#                            _external=True),
-#        },
-#        'resource': {
-#            'href': url_for('resource', uuid=r['uuid'],
-#                            _external=True)
-#        },
     }
 
     return n
@@ -70,18 +54,6 @@ def add_relationship_data(n):
             'href': url_for('relationship', uuid=uuid,
                             _external=True),
         },
-#        'revisions': {
-#            'href': url_for('relationship-revisions', uuid=uuid,
-#                            _external=True),
-#        },
-#        'timeline': {
-#            'href': url_for('relationship-timeline', uuid=uuid,
-#                            _external=True)
-#        },
-#        'resource': {
-#            'href': url_for('resource', uuid=r['uuid'],
-#                            _external=True)
-#        },
     }
 
     return n
