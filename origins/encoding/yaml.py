8b7538a156029f74cb4e13171ee9d2cdd51c61e0
Byron Ruth <b@devel.io>
2014-09-07 20:10:57 -0400

    Add pyyaml and redis as hard install requirements
    Signed-off-by: Byron Ruth <b@devel.io>

--- origins/encoding/yaml.py
+++ origins/encoding/yaml.py
@@ -1,18 +1,11 @@
+import yaml
 from io import StringIO
-
-try:
-    import yaml
-except ImportError:
-    pass
 
 
 def load(f, resource=None):
     """Takes a YAML-encoded file-like object and decodes it into the native
     resource structure.
     """
-    if not yaml:
-        raise EnvironmentError('PyYAML not installed')
-
     data = yaml.load(f)
 
     if resource is not None:
