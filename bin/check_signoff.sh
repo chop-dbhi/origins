e4fe9a7b99c73e757bb30cf45d919f64e3937159
Byron Ruth <b@devel.io>
2015-01-03 20:41:36 -0500

    Update Travis config and signoff check
    sudo was disabled to run in container-based environment.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- bin/check_signoff.sh
+++ bin/check_signoff.sh
@@ -1,19 +1,76 @@
-#!/bin/sh
+#!/bin/bash
 
-env | sort
+VALIDATE_REPO='https://github.com/chop-dbhi/origins.git'
+VALIDATE_BRANCH='master'
 
-sudo apt-get update -qq
-sudo apt-get install -qq python-yaml
+if [ -z "$VALIDATE_UPSTREAM" ]; then
+    # this is kind of an expensive check, so let's not do this twice if we
+    # are running more than one validate bundlescript
+       
+    if [ "$TRAVIS" = 'true' -a "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+        VALIDATE_REPO="https://github.com/${TRAVIS_REPO_SLUG}.git"
+        VALIDATE_BRANCH="${TRAVIS_BRANCH}"
+    fi
+    
+    VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
+    
+    git fetch -q "$VALIDATE_REPO" "refs/heads/$VALIDATE_BRANCH"
+    VALIDATE_UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
+    
+    VALIDATE_COMMIT_LOG="$VALIDATE_UPSTREAM..$VALIDATE_HEAD"
+    VALIDATE_COMMIT_DIFF="$VALIDATE_UPSTREAM...$VALIDATE_HEAD"
+    
+    validate_diff() {
+        if [ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]; then
+            git diff "$VALIDATE_COMMIT_DIFF" "$@"
+        fi
+    }
+    validate_log() {
+        if [ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]; then
+            git log "$VALIDATE_COMMIT_LOG" "$@"
+        fi
+    }
+fi
 
-git remote add upstream git://github.com/cbmi/origins.git
+adds=$(validate_diff --numstat | awk '{ s += $1 } END { print s }')
+dels=$(validate_diff --numstat | awk '{ s += $2 } END { print s }')
 
-UPSTREAM=master
-DIRNAME=$( cd "$( dirname "$0" )" && pwd )
+: ${adds:=0}
+: ${dels:=0}
 
-if [ "$TRAVIS_PULL_REQUEST" != false ]; then
-    UPSTREAM=$TRAVIS_BRANCH;
-fi;
+dcoPrefix='Signed-off-by:'
 
-git fetch --append --no-tags upstream refs/heads/$UPSTREAM:refs/remotes/upstream/$UPSTREAM
+check_dco() {
+    grep -qE "$dcoPrefix"
+}
 
-/usr/bin/python "$DIRNAME/check_signoff.py"
+if [ $adds -eq 0 -a $dels -eq 0 ]; then
+    echo '0 adds, 0 deletions; nothing to validate!'
+else
+    commits=( $(validate_log --format='format:%H%n') )
+    badCommits=()
+
+    for commit in "${commits[@]}"; do
+        if [ -z "$(git log -1 --format='format:' --name-status "$commit")" ]; then
+            # no content (ie, Merge commit, etc)
+            continue
+        fi
+        if ! git log -1 --format='format:%B' "$commit" | check_dco; then
+            badCommits+=( "$commit" )
+        fi
+    done
+    if [ ${#badCommits[@]} -eq 0 ]; then
+        echo "Congratulations!  All commits are properly signed off!"
+    else
+        {
+            echo "These commits do not have a proper '$dcoPrefix' marker:"
+            for commit in "${badCommits[@]}"; do
+                echo " - $commit"
+            done
+            echo
+            echo 'Please amend each commit with a sign-off.'
+            echo
+        } >&2
+        false
+    fi
+fi
