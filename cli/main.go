9c1380bfc8d292864207ad25934c730e20dacbc7
Byron Ruth <b@devel.io>
2015-03-17 09:04:48 -0400

    Add support for a config file
    This is purely for convenience and removes the need to specify the
    required global options for the storage engine.
    
    By default, if the file named `origins.json` or `origins.yml` exists in
    the current working directory, it will be loaded automatically.
    Otherwise the `--config` argument can be used to specify an alternate
    location of the file.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- cli/main.go
+++ cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -32,15 +33,27 @@ func main() {
 	viper.SetEnvPrefix("ORIGINS_CLI")
 	viper.AutomaticEnv()
 
+	// Default locations for the origins config file.
+	viper.SetConfigName("origins")
+
+	// Directory the program is being called from
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err == nil {
+		viper.AddConfigPath(dir)
+	}
+
 	flags := mainCmd.PersistentFlags()
 
 	flags.String("loglevel", "info", "Level of log messages to emit. Choices are: debug, info, warn, error, fatal, panic.")
 	flags.String("path", "", "Path to a file or directory filesystem-based storage backends.")
 	flags.String("storage", "", "Storage backend. Choices are: boltdb, diskv, sqlite, and memory.")
+	flags.String("config", "", "Path to config file.")
 
 	viper.BindPFlag("loglevel", flags.Lookup("loglevel"))
 	viper.BindPFlag("path", flags.Lookup("path"))
 	viper.BindPFlag("storage", flags.Lookup("storage"))
+	viper.BindPFlag("config", flags.Lookup("config"))
 
 	// Turn on debugging for all commands.
 	mainCmd.ParseFlags(os.Args)
@@ -53,6 +66,15 @@ func main() {
 	}
 
 	logrus.SetLevel(level)
+
+	config := viper.GetString("config")
+
+	if config != "" {
+		viper.SetConfigFile(config)
+	}
+
+	// Read configuration file if present.
+	viper.ReadInConfig()
 
 	mainCmd.Execute()
 }
