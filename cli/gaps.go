9c8368701d8590da5060b19aca861bbf10cabcc8
Byron Ruth <b@devel.io>
2015-03-23 21:27:54 -0700

    Implement gaps command
    Fix #109
    
    Signed-off-by: Byron Ruth <b@devel.io>

---
+++ cli/gaps.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/chop-dbhi/origins/view"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var gapsCmd = &cobra.Command{
+	Use: "gaps <domain>",
+
+	Short: "Print a list of entity/attribute pairs with gaps in a domain.",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Usage()
+			os.Exit(1)
+		}
+
+		s := viper.GetString("gaps.threshold")
+		d, err := time.ParseDuration(s)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing threshold as a duration.")
+			cmd.Usage()
+			os.Exit(1)
+		}
+
+		store := initStore()
+
+		v := view.Now(store)
+		dv := v.Domain(args[0])
+
+		var (
+			b []byte
+		)
+
+		for _, gs := range dv.Gaps(d) {
+			b, err = json.MarshalIndent(gs, "", "\t")
+
+			if err != nil {
+				logrus.Error(err)
+				continue
+			}
+
+			fmt.Println(string(b))
+		}
+	},
+}
+
+func init() {
+	flags := gapsCmd.Flags()
+
+	flags.String("threshold", "1s", "The maximum duration allowed between a retraction and assertion.")
+
+	viper.BindPFlag("gaps.threshold", flags.Lookup("threshold"))
+}
