1fc1b3a75b009d20dc1ac9898fab662cff5c538c
Byron Ruth <b@devel.io>
2015-03-27 21:46:40 -0700

    Add support for taking multiple paths for load command
    Signed-off-by: Byron Ruth <b@devel.io>

--- cli/load.go
+++ cli/load.go
@@ -6,25 +6,51 @@ import (
 	"os"
 
 	"github.com/chop-dbhi/origins/fact"
+	"github.com/chop-dbhi/origins/storage"
 	"github.com/chop-dbhi/origins/transactor"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+func loadFile(store *storage.Store, domain string, format string, fake bool, r io.Reader) {
+	// Wrap in a reader to handle carriage returns.
+	r = &UniversalReader{r}
+
+	var reader fact.Reader
+
+	switch format {
+	case "csv":
+		reader = fact.CSVReader(r)
+	case "jsonstream":
+		reader = fact.JSONStreamReader(r)
+	default:
+		fmt.Printf("Unknown format %s\n", format)
+		os.Exit(1)
+	}
+
+	results, err := transactor.Transact(store, reader, domain, false, !fake)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, r := range results {
+		fmt.Println(r)
+	}
+}
+
 var loadCmd = &cobra.Command{
-	Use: "load [path]",
+	Use: "load [path, ...]",
 
-	Short: "Loads facts to the store.",
+	Short: "Loads facts into the store.",
 
-	Long: `load takes a path to a file or "-" to read from stdin.`,
+	Long: `load takes one or more paths to read or "-" to read from stdin.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		store := initStore()
 
 		var (
-			r   io.Reader
-			err error
-
 			format = viper.GetString("load.format")
 			domain = viper.GetString("load.domain")
 			fake   = viper.GetBool("load.fake")
@@ -32,44 +58,21 @@ var loadCmd = &cobra.Command{
 
 		// No path provided, use stdin.
 		if len(args) == 0 {
-			r = os.Stdin
-		} else {
-			f, err := os.Open(args[0])
+			loadFile(store, domain, format, fake, os.Stdin)
+			return
+		}
+
+		for _, fn := range args {
+			file, err := os.Open(fn)
 
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			defer f.Close()
+			defer file.Close()
 
-			r = f
-		}
-
-		// Wrap in a reader to handle carriage returns.
-		r = &UniversalReader{r}
-
-		var reader fact.Reader
-
-		switch format {
-		case "csv":
-			reader = fact.CSVReader(r)
-		case "jsonstream":
-			reader = fact.JSONStreamReader(r)
-		default:
-			fmt.Printf("Unknown format %s\n", format)
-			os.Exit(1)
-		}
-
-		results, err := transactor.Transact(store, reader, domain, true, !fake)
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		for _, r := range results {
-			fmt.Println(r)
+			loadFile(store, domain, format, fake, file)
 		}
 	},
 }
