7a81cb1c64d251cf6cf9d1bba05e393e3b0b81d8
Byron Ruth <b@devel.io>
2015-04-08 16:53:30 -0400

    Add support for loading compressed files
    Supported compression formats include gzip and bzip2. In addition,
    the file format and compression method is detected for file-based
    loads. stdin loads require the --format argument to be specified.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- cli/load.go
+++ cli/load.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"compress/bzip2"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 
 	"github.com/chop-dbhi/origins/fact"
 	"github.com/chop-dbhi/origins/storage"
 	"github.com/chop-dbhi/origins/transactor"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-func loadFile(store *storage.Store, r io.Reader) {
-
+func loadFile(store *storage.Store, r io.Reader, format, compression string) {
 	var (
 		err error
 
-		format = viper.GetString("load.format")
 		domain = viper.GetString("load.domain")
 		fake   = viper.GetBool("load.fake")
 		strict = viper.GetBool("load.strict")
 	)
 
-	// Wrap in a reader to handle carriage returns.
+	// Apply decompression.
+	if compression != "" {
+		logrus.Debugf("Applying %s decompression", compression)
+
+		switch compression {
+		case "bzip2":
+			r = bzip2.NewReader(r)
+		case "gzip":
+			if r, err = gzip.NewReader(r); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		default:
+			fmt.Printf("Unsupported compression format %s\n", compression)
+			os.Exit(1)
+		}
+	}
+
+	// Wrap in a reader to handle carriage returns before passing
+	// it into the CSV reader.
 	r = &UniversalReader{r}
 
 	var reader fact.Reader
@@ -31,10 +52,10 @@ func loadFile(store *storage.Store, r io.Reader) {
 	switch format {
 	case "csv":
 		reader = fact.CSVReader(r)
-	case "jsonstream":
+	case "jsonstream", "jsons":
 		reader = fact.JSONStreamReader(r)
 	default:
-		fmt.Printf("Unknown format %s\n", format)
+		fmt.Printf("Unsupported file format %s\n", format)
 		os.Exit(1)
 	}
 
@@ -56,6 +77,32 @@ func loadFile(store *storage.Store, r io.Reader) {
 	}
 }
 
+// detectFormat detects the file format based on the filename.
+func detectFormat(n string) string {
+	var ok bool
+
+	if ok, _ = regexp.MatchString(`(?i)\.csv\b`, n); ok {
+		return "csv"
+	} else if ok, _ = regexp.MatchString(`(?i)\.jsonstream\b`, n); ok {
+		return "jsonstream"
+	}
+
+	return ""
+}
+
+// detectCompression detects the file compression type based on the filename.
+func detectCompression(n string) string {
+	var ok bool
+
+	if ok, _ = regexp.MatchString(`(?i)\.gz\b`, n); ok {
+		return "gzip"
+	} else if ok, _ = regexp.MatchString(`(?i)\.(bzip2|bz2)\b`, n); ok {
+		return "bzip2"
+	}
+
+	return ""
+}
+
 var loadCmd = &cobra.Command{
 	Use: "load [path, ...]",
 
@@ -66,13 +113,28 @@ var loadCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		store := initStore()
 
+		format := viper.GetString("load.format")
+		compression := viper.GetString("load.compression")
+
 		// No path provided, use stdin.
 		if len(args) == 0 {
-			loadFile(store, os.Stdin)
+			loadFile(store, os.Stdin, format, compression)
 			return
 		}
 
 		for _, fn := range args {
+			// Reset to initial value
+			format = viper.GetString("load.format")
+			compression = viper.GetString("load.compression")
+
+			if format == "" {
+				format = detectFormat(fn)
+			}
+
+			if compression == "" {
+				compression = detectCompression(fn)
+			}
+
 			file, err := os.Open(fn)
 
 			if err != nil {
@@ -82,7 +144,7 @@ var loadCmd = &cobra.Command{
 
 			defer file.Close()
 
-			loadFile(store, file)
+			loadFile(store, file, format, compression)
 		}
 	},
 }
@@ -90,12 +152,14 @@ var loadCmd = &cobra.Command{
 func init() {
 	flags := loadCmd.Flags()
 
-	flags.String("format", "csv", "Format of the facts being written to the store. Choices are: csv, jsonstream")
+	flags.String("format", "", "Format of the facts being written to the store. Choices are: csv, jsonstream")
+	flags.String("compression", "", "Compression method used on the facts being loaded to the store. Choices are: bzip2, gzip")
 	flags.String("domain", "", "Domain to load the facts to. If not supplied, the fact domain attribute must be defined.")
 	flags.Bool("fake", false, "Set to prevent data from being written to the store.")
 	flags.Bool("strict", false, "When true and a default domain is specified, the fact domain must match.")
 
 	viper.BindPFlag("load.format", flags.Lookup("format"))
+	viper.BindPFlag("load.compression", flags.Lookup("compression"))
 	viper.BindPFlag("load.domain", flags.Lookup("domain"))
 	viper.BindPFlag("load.fake", flags.Lookup("fake"))
 	viper.BindPFlag("load.strict", flags.Lookup("strict"))
